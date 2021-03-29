9b79704d72127a47a94af3a6b4c7e2c9df535880
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-29 17:16:34 +0300

    Delete Item functions are implemented

--- budget-app/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ budget-app/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
@@ -9,10 +9,16 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
 export class BudgetItemCardComponent implements OnInit {
 
   @Input() item: BudgetItem;  
-
+  @Output() xButtonClick:  EventEmitter<any> = new EventEmitter<any>();
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onXButtonClick () {
+    // Here we want to get an evet
+    this.xButtonClick.emit()
   }
 
 }
